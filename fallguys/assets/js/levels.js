e0b8c278202bb693b4924b2fe49960e6b3b9547c
Wouter Van Schandevijl <woutervs@hotmail.com>
2021-09-05 21:03:24 +0200

    levels: slimescraper, slime climb & hoverboard heroes

--- fallguys/assets/js/levels.js
+++ fallguys/assets/js/levels.js
@@ -3,4 +3,21 @@ $(function() {
         $('.flybywrapper').hide();
         $('.flyby-movie').show();
     });
+
+
+    $('.level-detail .btn-group > button').click(function() {
+        var self = $(this);
+
+        var allButtons = $('button', self.parent());
+        allButtons.each(function() {
+            $(this).removeClass('btn-secondary').addClass('btn-light');
+
+            var id = $(this).data('id');
+            $('.' + id).addClass('hidden');
+        });
+
+        var id = self.data('id');
+        $('.' + id).removeClass('hidden');
+        self.addClass('btn-secondary').removeClass('btn-light');
+    });
 });
